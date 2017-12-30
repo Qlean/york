32ee8760e4f9420017ca2291e6e76099d432e25a
Vadim Shvetsov <vadim.shvetsov@gmail.com>
2017-12-30 16:37:17 +0300

    ops-682 Add colors to york-core

---
+++ packages/york-core/src/styles/colors.js
@@ -0,0 +1,12 @@
+export default {
+  elfGreen: '#20A052',
+  gorse: '#FAE12E',
+  persianRed: '#D13737',
+  white: '#FFFFFF',
+  whiteSmoke: '#F8F8F8',
+  gainsboro: '#D9D9D9',
+  darkGrey: '#A6A6A6',
+  mortar: '#595959',
+  black: '#000000',
+  nero: '#222222',
+};
