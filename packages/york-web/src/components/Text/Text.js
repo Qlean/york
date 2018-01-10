04c6d6bb799a0ce0ff3acb5469fbedc792ee4c09
Vadim Shvetsov <vadim.shvetsov@gmail.com>
2018-01-10 23:05:08 +0300

    ops-683 Add Text presets, tests and extend Text with text-transform, remove fonts presets

--- packages/york-web/src/components/Text/Text.js
+++ packages/york-web/src/components/Text/Text.js
@@ -1,27 +1,103 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { colors } from 'york-core';
-import fonts from '../../styles/fonts';
 
-const Text = styled.span`
-  ${({ fontWeight }) => fonts[fontWeight]}
+const presets = {
+  ph1: {
+    fontSize: 70,
+    lineHeight: 75,
+    fontWeight: 700,
+  },
+  ph2: {
+    fontSize: 60,
+    lineHeight: 65,
+    fontWeight: 700,
+  },
+  ph3: {
+    fontSize: 50,
+    lineHeight: 55,
+    fontWeight: 700,
+  },
+  h1: {
+    fontSize: 40,
+    lineHeight: 45,
+    fontWeight: 700,
+  },
+  h2: {
+    fontSize: 30,
+    lineHeight: 35,
+    fontWeight: 700,
+  },
+  h3: {
+    fontSize: 25,
+    lineHeight: 30,
+    fontWeight: 700,
+  },
+  h4: {
+    fontSize: 20,
+    lineHeight: 25,
+    fontWeight: 700,
+  },
+  text1: {
+    fontSize: 20,
+    lineHeight: 30,
+    fontWeight: 500,
+  },
+  text2: {
+    fontSize: 16,
+    lineHeight: 25,
+    fontWeight: 500,
+  },
+  link: {
+    fontSize: 15,
+    lineHeight: 25,
+    fontWeight: 700,
+    textTransform: 'uppercase',
+  },
+  caption1: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 500,
+  },
+  caption2: {
+    fontSize: 12,
+    lineHeight: 15,
+    fontWeight: 500,
+  },
+};
+
+const StyledText = styled.span`
+  font-family: MuseoSansCyrl;
+  font-weight: ${({ fontWeight }) => fontWeight};
   font-size: ${({ fontSize }) => `${fontSize}px`};
   line-height: ${({ lineHeight }) => `${lineHeight}px`};
   color: ${({ color }) => colors[color] || color};
+  ${({ fontStyle }) => fontStyle && `font-style: ${fontStyle};`}
+  ${({ textTransform }) => textTransform && `text-transform: ${textTransform};`}
 `;
 
-Text.defaultProps = {
-  fontWeight: 'normal',
-  fontSize: 16,
-  lineHeight: 25,
+StyledText.defaultProps = {
   color: 'coal',
 };
 
-Text.propTypes = {
-  fontWeight: PropTypes.oneOf(['bold', 'normal', 'light']),
-  fontSize: PropTypes.number,
-  lineHeight: PropTypes.number,
+StyledText.propTypes = {
+  fontWeight: PropTypes.number.isRequired,
+  fontSize: PropTypes.number.isRequired,
+  lineHeight: PropTypes.number.isRequired,
   color: PropTypes.string,
+  fontStyle: PropTypes.oneOf(['normal', 'italic']),
+  textTransform: PropTypes.oneOf(['uppercase', 'lowercase', 'capitalize', 'none']),
+};
+
+const Text = ({ preset, ...rest }) => (<StyledText {...presets[preset]} {...rest}/>);
+
+Text.defaultProps = {
+  preset: 'text2',
+};
+
+Text.propTypes = {
+  preset: PropTypes.oneOf(Object.keys(presets)),
 };
 
 export default Text;
--- packages/york-web/src/index.js
+++ packages/york-web/src/index.js
@@ -1,1 +1,3 @@
+import './styles/fonts/setupFonts';
+
 export { default as Text } from './components/Text/Text'; //eslint-disable-line
