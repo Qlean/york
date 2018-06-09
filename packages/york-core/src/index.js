b1a34966b538ae3b88b9b6383be3e771df3ce284
Alex Tewpin <alex.tewpin@gmail.com>
2018-06-09 17:47:56 +0300

    Revert to 0.3.2

---
+++ packages/york-core/src/index.js
@@ -0,0 +1,5 @@
+export { default as colors } from './styles/colors';
+
+export { default as capitalize } from './utils/capitalize';
+export { default as formatPhoneForHref } from './utils/formatPhoneForHref';
+export { default as formatServerError } from './utils/formatServerError';
--- packages/york-core/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as capitalize } from './capitalize';
-export { default as formatPhoneForHref } from './formatPhoneForHref';
-export { default as formatServerError } from './formatServerError';
