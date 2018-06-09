710841a94020bfc86e0555336f63df02d25392ca
Alex Tewpin <alex.tewpin@gmail.com>
2018-06-09 17:11:46 +0300

    Splitted import namespaces

--- packages/york-core/src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as colors } from './styles/colors';
-
-export { default as capitalize } from './utils/capitalize';
-export { default as formatPhoneForHref } from './utils/formatPhoneForHref';
-export { default as formatServerError } from './utils/formatServerError';
---
+++ packages/york-core/src/utils/index.js
@@ -0,0 +1,3 @@
+export { default as capitalize } from './capitalize';
+export { default as formatPhoneForHref } from './formatPhoneForHref';
+export { default as formatServerError } from './formatServerError';
