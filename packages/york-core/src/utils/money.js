60483704e6b37f09c641dcb70dbb667dba9a5d9b
Stanislav Ustiuzhanin <ov.s.ust@gmail.com>
2018-08-07 13:54:33 +0300

    add utils

---
+++ packages/york-core/src/utils/money.js
@@ -0,0 +1,37 @@
+import format from './format';
+
+
+// @param {Number} value
+
+function curryFormat(value) {
+
+  // @param {String} formatter
+
+  return (formatter = 'money') => {
+    return format(formatter, value);
+  }
+}
+
+
+// @param {Number} value
+// @param {Number} discount
+
+function addDiscount(value, discount) {
+  const newValue = Math.ceil(value * (1 - discount / 100));
+
+  return newValue > 0 ? newValue : 0;
+}
+
+
+// @param {Number} value
+// @param {Number|Array} discounts
+
+export default function money(value, discounts = 0) {
+  if (Array.isArray(discounts)) {
+    return curryFormat(discounts.reduce((acc, discount) => {
+      return addDiscount(acc, discount);
+    }, value));
+  }
+
+  return curryFormat(addDiscount(value, discounts));
+}
