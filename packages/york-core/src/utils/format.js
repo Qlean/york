6b3c20c659cf9c27e376dbb73b8c1f36eec564e5
Stanislav Ustiuzhanin <ov.s.ust@gmail.com>
2018-08-08 10:31:53 +0300

    tech-90-merge-greyhound (#13)

---
+++ packages/york-core/src/utils/format.js
@@ -0,0 +1,14 @@
+const getFormattedMoney = (number) => {
+  const formattedNumber = new Intl.NumberFormat('ru-RU').format(number / 100);
+  const suffix = 'р.';
+
+  return `${formattedNumber}\u00a0${suffix}`;
+};
+
+const formattersMap = {
+  money: getFormattedMoney
+};
+
+export default function format(formatter, value) {
+  return formattersMap[formatter](value);
+};
