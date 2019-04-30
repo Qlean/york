1680a4c4c62ee317a02792390a2a9adcfb5a3454
mlnchk <danil.mlnchk@gmail.com>
2019-04-30 20:16:21 +0300

    Update button readme

--- packages/york-react-native/src/components/Button/index.js
+++ packages/york-react-native/src/components/Button/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { colors } from '@qlean/york-core';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { colors } from '@qlean/york-core'
 
 const sizes = {
   m: {
     height: 50,
-    width: '100%', // FIXME: no percents
     borderRadius: 4,
   },
   s: {
@@ -14,70 +13,84 @@ const sizes = {
     width: 120,
     borderRadius: 4,
   },
-};
+}
 
 const presets = {
   green: {
-    button: { backgroundColor: colors.green },
+    button: { backgroundColor: colors.green, borderColor: colors.green },
   },
   black: {
-    button: { backgroundColor: colors.black },
+    button: { backgroundColor: colors.black, borderColor: colors.black },
+  },
+  grey: {
+    button: { backgroundColor: '#EEEEEE', borderColor: '#EEEEEE' },
+    text: { color: colors.coal },
   },
   greenLinear: {
-    button: {
-      backgroundColor: colors.white,
-      borderColor: colors.green,
-      borderWidth: 1,
-    },
+    button: { backgroundColor: colors.white, borderColor: colors.green },
     text: { color: colors.green },
   },
-  blackLinear: {
-    button: {
-      backgroundColor: colors.white,
-      borderColor: colors.black,
-      borderWidth: 1,
-    },
-    text: {
-      color: colors.black,
-    },
+  greyLinear: {
+    button: { backgroundColor: colors.white, borderColor: '#EEEEEE' },
+    text: { color: colors.coal },
   },
-};
+}
 
 const style = StyleSheet.create({
   main: { alignItems: 'center', justifyContent: 'center' },
   text: { color: colors.white },
   disabled: { backgroundColor: colors.smoke, borderColor: colors.smoke },
-  disabledText: { color: colors.silver },
+  disabledText: { color: colors.grey },
   s: { ...sizes.s },
   m: { ...sizes.m },
   green: { ...presets.green.button },
   black: { ...presets.black.button },
+  grey: { ...presets.grey.button },
+  greyText: { ...presets.grey.text },
   greenLinear: { ...presets.greenLinear.button },
   greenLinearText: { ...presets.greenLinear.text },
-  blackLinear: { ...presets.blackLinear.button },
-  blackLinearText: { ...presets.blackLinear.text },
-  text1: {},
-  greenRound: {},
-  yellowRound: {},
-});
+  greyLinear: { ...presets.greyLinear.button },
+  greyLinearText: { ...presets.greyLinear.text },
+})
 
 /**
  * Компонент кнопки.
  */
-const Button = ({ children, isDisabled, preset, size = "m" }) => (
+const Button = ({ children, isDisabled, preset, size }) => (
   <TouchableOpacity
-    style={[style.main, style[size], style[preset], isDisabled && style.disabled]}
+    style={[
+      style.main,
+      style[size],
+      style[preset],
+      isDisabled && style.disabled,
+    ]}
     disabled={isDisabled}
+    activeOpacity={0.8}
   >
-    <Text style={[style.text, style[`${preset}Text`], isDisabled && style.disabledText]}>
+    <Text
+      style={[
+        style.text,
+        style[`${preset}Text`],
+        isDisabled && style.disabledText,
+      ]}
+    >
       {children}
     </Text>
   </TouchableOpacity>
-);
+)
 
-Button.propTypes = {
-  /** Управление preset самой кнопки. */
-  preset: PropTypes.oneOf(Object.keys(presets)),
+Button.defaultProps = {
+  preset: 'green',
+  size: 'm',
 }
 
-export default Button;
+Button.propTypes = {
+  /** Доступна ли кнопка для нажатия */
+  isDisabled: PropTypes.bool,
+  /** Пресет кнопки */
+  preset: PropTypes.oneOf(Object.keys(presets)),
+  /** Размер кнопки */
+  size: PropTypes.oneOf(Object.keys(sizes)),
+}
+
+export default Button
