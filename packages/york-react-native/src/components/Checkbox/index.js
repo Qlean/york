ed486a42cb06c30d4985d046d30bb0e42dffd85f
Danil <danil.mlnchk@gmail.com>
2019-09-02 13:38:52 +0300

    sup-888-checkbox-radio (#74)
    * sup-888-checkbox-radio
    
    * Pngs
    
    * Review fixes
    
    * Replace assets
    
    * Review fixes

---
+++ packages/york-react-native/src/components/Checkbox/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TouchableOpacity, StyleSheet, View, Image } from 'react-native'
+import { colors } from '@qlean/york-core'
+
+import {
+  Text,
+  Separator,
+  sizes,
+  borderRadiuses,
+} from '@qlean/york-react-native'
+
+const checkboxSize = sizes[4]
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  checkbox: {
+    width: checkboxSize,
+    height: checkboxSize,
+    borderRadius: borderRadiuses.small,
+    borderColor: colors.silver,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    /** Компенсация разницы размера чекбокса и lineHeight */
+    marginTop: 2,
+  },
+  checkboxChecked: {
+    borderColor: colors.green,
+    backgroundColor: colors.green,
+  },
+  checkboxWithError: {
+    borderColor: colors.red,
+  },
+  textContainer: {
+    flexShrink: 1,
+  },
+})
+
+/**
+ * Чекбокс. Используется для выбора булевых значений.
+ */
+const Checkbox = ({ title, caption, error, name, value, onChange }) => {
+  const onPress = () => onChange(!value)
+  return (
+    <TouchableOpacity style={styles.root} onPress={onPress} testID={name}>
+      <View
+        style={[
+          styles.checkbox,
+          error && styles.checkboxWithError,
+          value && styles.checkboxChecked,
+        ]}
+      >
+        <Image source={require('./assets/check.png')} />
+      </View>
+      <Separator width={2} />
+      <View style={styles.textContainer}>
+        <Text>{title}</Text>
+        {caption && <Text color="grey">{caption}</Text>}
+        {error && <Text color="red">{error}</Text>}
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+Checkbox.defaultProps = {
+  caption: null,
+  error: null,
+}
+
+Checkbox.propTypes = {
+  /** Лэйбл чекбокса */
+  title: PropTypes.string.isRequired,
+  /** Подзаголовок чекбокса */
+  caption: PropTypes.string,
+  /** Текст ошибки */
+  error: PropTypes.string,
+  /** Имя для автотестов, прокидывается как testID */
+  name: PropTypes.string.isRequired,
+  /** Выбран ли чекбокс */
+  value: PropTypes.bool.isRequired,
+  /** Коллбек, вызываемый при изменении значения с аргументом `value` */
+  onChange: PropTypes.func.isRequired,
+}
+
+export default Checkbox
---
+++ packages/york-react-native/src/components/RadioGroup/index.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
+import { colors } from '@qlean/york-core'
+import { Text, Separator, sizes } from '@qlean/york-react-native'
+
+const bulletSize = sizes[4]
+
+const styles = StyleSheet.create({
+  option: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  bullet: {
+    width: bulletSize,
+    height: bulletSize,
+    borderRadius: bulletSize / 2,
+    borderWidth: 1,
+    borderColor: colors.silver,
+    backgroundColor: colors.white,
+    /** Компенсация разницы размера буллета и lineHeight */
+    marginTop: 2,
+  },
+  bulletSelected: {
+    borderWidth: 6,
+    borderColor: colors.green,
+  },
+  textContainer: {
+    flexShrink: 0,
+  },
+})
+
+const RadioItem = ({ title, caption, name, isSelected, onPress }) => (
+  <TouchableOpacity style={styles.option} onPress={onPress} testID={name}>
+    <View style={[styles.bullet, isSelected && styles.bulletSelected]} />
+    <Separator width={2} />
+    <View style={styles.textContainer}>
+      <Text>{title}</Text>
+      {caption ? <Text color="grey">{caption}</Text> : null}
+    </View>
+  </TouchableOpacity>
+)
+
+RadioItem.defaultProps = {
+  caption: null,
+}
+
+RadioItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  caption: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+}
+
+const keyExtractor = ({ value }) => String(value)
+const ItemSeparatorComponent = () => <Separator height={4} />
+
+/**
+ * Группа радио-инпутов, используется для выбора одной опции из нескольких.
+ */
+const RadioGroup = ({ options, value, name, onChange }) => {
+  const onItemPress = (...args) => () => onChange(...args)
+  const renderItem = args => {
+    const {
+      item: { title, value: itemValue, caption },
+      index,
+    } = args
+    return (
+      <RadioItem
+        title={title}
+        caption={caption}
+        name={`${name}/${itemValue}`}
+        isSelected={itemValue === value}
+        onPress={onItemPress(itemValue, index)}
+      />
+    )
+  }
+  return (
+    <FlatList
+      scrollEnabled={false}
+      data={options}
+      keyExtractor={keyExtractor}
+      ItemSeparatorComponent={ItemSeparatorComponent}
+      renderItem={renderItem}
+      extraData={value}
+    />
+  )
+}
+
+RadioGroup.defaultProps = {
+  value: null,
+}
+
+RadioGroup.propTypes = {
+  /** Имя для автотестов, прокидывается как testID в каждый элемент группы в формате `name/value` */
+  name: PropTypes.string.isRequired,
+  /** Список опций */
+  options: PropTypes.arrayOf(
+    PropTypes.exact({
+      /** Значение опции */
+      value: PropTypes.oneOfType([
+        PropTypes.string.isRequired,
+        PropTypes.number.isRequired,
+      ]).isRequired,
+      /** Лейбл опции */
+      title: PropTypes.string.isRequired,
+      /** Подзаголовок опции */
+      caption: PropTypes.string,
+    }).isRequired,
+  ).isRequired,
+  /** Выбранная опция */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /** Коллбек, вызываемый при изменении значения с аргументами `value` и `index` */
+  onChange: PropTypes.func.isRequired,
+}
+
+export default RadioGroup
