66451981ad7069442b912baa7680d2c1f4820b06
itslooklike <delsol@bk.ru>
2019-09-02 16:00:16 +0300

    more mobile

--- packages/york-web/src/components/complex/Header/MobileHeader/index.js
+++ packages/york-web/src/components/complex/Header/MobileHeader/index.js
@@ -12,7 +12,7 @@ const Root = styled.header`
   background-color: ${colors.white};
 `
 
-const Inner = styled.div`
+const LevelOneContainer = styled.div`
   display: flex;
   align-items: center;
   padding-left: 20px;
@@ -53,13 +53,69 @@ const Burger = styled(IconBurger)`
   ${({ isOpen }) => (isOpen ? 'transform: rotate(-270deg);' : '')}
 `
 
+const ScrollerContainer = styled.div`
+  position: relative;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 40px;
+    background-image: linear-gradient(
+      270deg,
+      rgba(255, 255, 255, 0.85) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    pointer-events: none;
+  }
+`
+
+const Scroller = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const LevelTwoMenuItem = styled.div`
+  flex-shrink: 0;
+  padding: 12px 10px 13px;
+  color: ${({ isActive }) => (isActive ? colors.green : colors.coal)};
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 20px;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:first-child {
+    padding-left: 20px;
+  }
+`
+
+const LevelThreeMenuItem = styled(LevelTwoMenuItem)`
+  font-weight: normal;
+  text-transform: initial;
+  letter-spacing: 0.4px;
+`
+
 export default function MobileHeader(props) {
+  const { logo, levelOneMenu, levelTwoMenu } = props
   const [burgerActive, toggleBurger] = React.useState(false)
-  const { logo, levelOneMenu } = props
+  const [activeLevelTwoMenu, setLevelTwoMenu] = React.useState(0)
+  const [activeLevelThreeMenu, setLevelThreeMenu] = React.useState(0)
+  const resetLevelThreeMenu = React.useCallback(() => setLevelThreeMenu(0))
+  const levelThreeMenu = levelTwoMenu[activeLevelTwoMenu].subMenu
 
   return (
     <Root>
-      <Inner>
+      <LevelOneContainer>
         <LeftSlot>
           <Logo src={logo.url} alt={logo.alt} />
           <GeolocationWrap>
@@ -80,7 +136,38 @@ export default function MobileHeader(props) {
             <Burger isOpen={burgerActive} />
           </BurgerButton>
         </RightSlot>
-      </Inner>
+      </LevelOneContainer>
+      <ScrollerContainer>
+        <Scroller>
+          {levelTwoMenu.map((menuItem, idx) => (
+            <LevelTwoMenuItem
+              key={menuItem.title}
+              isActive={idx === activeLevelTwoMenu}
+              onClick={() => {
+                setLevelTwoMenu(idx)
+                resetLevelThreeMenu()
+              }}
+            >
+              {menuItem.title}
+            </LevelTwoMenuItem>
+          ))}
+        </Scroller>
+      </ScrollerContainer>
+      {levelThreeMenu && levelThreeMenu.length > 0 && (
+        <ScrollerContainer>
+          <Scroller>
+            {levelThreeMenu.map((menuItem, idx) => (
+              <LevelThreeMenuItem
+                key={menuItem.title}
+                isActive={idx === activeLevelThreeMenu}
+                onClick={() => setLevelThreeMenu(idx)}
+              >
+                {menuItem.title}
+              </LevelThreeMenuItem>
+            ))}
+          </Scroller>
+        </ScrollerContainer>
+      )}
     </Root>
   )
 }
--- packages/york-web/src/components/complex/Header/components/ClearedButton.js
+++ packages/york-web/src/components/complex/Header/components/ClearedButton.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const ClearedButton = styled.button`
-  margin: 0;
   padding: 0;
   border: 0;
+  margin: 0;
   background-color: transparent;
 `
 
