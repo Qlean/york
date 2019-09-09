101e1d8a315bb06bdda416f0f1898d2d8c18f4f5
levkus <l.ostanin@qlean.ru>
2019-09-09 19:02:52 +0300

    handle no context in useAnalytics

--- packages/york-analytics/src/hooks/useAnalytics.js
+++ packages/york-analytics/src/hooks/useAnalytics.js
@@ -6,12 +6,20 @@ const useAnalytics = category => {
   if (!category || typeof category !== 'string') {
     throw new Error('Error in `useAnalytics`: `category` must be a string')
   }
+  const analyticsContext = useContext(AnalyticsContext)
+  if (!analyticsContext) {
+    throw new Error(
+      'Error in `useAnalytics`: no analytics context found. `useAnalytics` should only be used inside `AnalyticsProvider`',
+    )
+  }
+
   const { trackEvent, analyticsRoute } = useContext(AnalyticsContext)
   if (!trackEvent) {
     throw new Error(
-      'Error in `useAnalytics`: no tracking function found in context. `useAnalytics should only be used inside `AnalyticsProvider`',
+      'Error in `useAnalytics`: no tracking function found in context. `useAnalytics` should only be used inside `AnalyticsProvider`',
     )
   }
+
   const memoizedTrackEvent = useCallback(
     data =>
       trackEvent({
