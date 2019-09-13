18b50b55311a52ccf0bbf01fc0ce24d1e21799ba
Vitaliy Pakshaev <pakshaev.vs@gmail.com>
2019-09-13 12:52:03 +0300

    Pagination

---
+++ packages/york-react-native/src/components/Pagination/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, StyleSheet } from 'react-native'
+import * as R from 'ramda'
+import { colors } from '@qlean/york-core'
+import { sizes } from '@qlean/york-react-native'
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dot: {
+    width: sizes[1],
+    height: sizes[1],
+    borderRadius: sizes[1] / 2,
+    backgroundColor: colors.whisper,
+  },
+  withRightMargin: {
+    marginRight: sizes[3],
+  },
+  filled: {
+    backgroundColor: colors.green,
+  },
+  selected: {
+    width: sizes[2],
+    height: sizes[2],
+    borderRadius: sizes[2] / 2,
+  },
+})
+
+/**
+ * Пагинация. Принимает количество страниц и номер текущей. Отсчёт начинается с 1
+ */
+const Stepper = ({ pagesCount, value: currentStep }) => {
+  const steps = R.times(n => n + 1, pagesCount)
+
+  return (
+    <View style={styles.root}>
+      {steps.map(step => (
+        <View
+          key={step}
+          style={[
+            styles.dot,
+            currentStep >= step && styles.filled,
+            currentStep === step && styles.selected,
+            step < pagesCount && styles.withRightMargin,
+          ]}
+        />
+      ))}
+    </View>
+  )
+}
+
+Stepper.propTypes = {
+  /** Текущая выбранная страница */
+  value: PropTypes.number.isRequired,
+  /** Количество страниц */
+  pagesCount: PropTypes.number.isRequired,
+}
+
+export default Stepper
