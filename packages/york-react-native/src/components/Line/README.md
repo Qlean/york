06a473d62f078e6e59ec8d70e82de1b8be05787d
Vitaliy Pakshaev <pakshaev.vs@gmail.com>
2019-09-16 16:25:25 +0300

    Line update

--- packages/york-react-native/src/components/Line/README.md
+++ packages/york-react-native/src/components/Line/README.md
@@ -1,5 +1,12 @@
 ```js
+import { Separator } from '@qlean/york-react-native'
 
 ;
+<>
+<Line color="red" />
+<Separator height={2}/>
+<Line color="jungle" />
+<Separator height={2}/>
 <Line />
+</>
 ```
--- packages/york-react-native/src/components/Line/index.js
+++ packages/york-react-native/src/components/Line/index.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { View, StyleSheet } from 'react-native'
 import { colors } from '@qlean/york-core'
 
 const styles = StyleSheet.create({
   line: {
-    borderBottomWidth: 1,
-    borderBottomColor: colors.whisper,
+    height: 1,
   },
 })
 
 /**
- * Линия. Декоративный элемент, не принимает пропов
+ * Линия. Декоративный элемент, по умолчанию занимает всю ширину своего контейнера
  */
-const Line = () => <View style={[styles.line]} />
+const Line = ({ color = colors, style }) => (
+  <View style={[styles.line, style, { backgroundColor: colors[color] }]} />
+)
+
+Line.defaultProps = {
+  color: 'whisper',
+  style: null,
+}
+
+Line.propTypes = {
+  /** Цвет линии */
+  color: PropTypes.string,
+  /** Дополнительные стили */
+  style: PropTypes.string,
+}
 
 export default Line
