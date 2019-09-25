6f85a6bce972b2db2adb06ec887ca925cf7327a7
levkus <l.ostanin@qlean.ru>
2019-09-25 12:27:46 +0300

    test client

--- packages/york-styleguide/components/Wrapper.js
+++ packages/york-styleguide/components/Wrapper.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { AnalyticsProvider } from '@qlean/york-analytics'
+import { Qat } from '@qleanlabs/analytics-client'
+
+const client = new Qat({
+  buffer: [],
+  options: {
+    url: 'https://anlt.cloud.qlean.ru/collect',
+    timeout: 5000,
+    wrapLink: false,
+  },
+})
 
 const trackEvent = ({ category, label, action, properties }) => {
+  console.log({
+    eventCategory: category,
+    eventLabel: label,
+    eventAction: action,
+    ...properties,
+  })
+
+  client.emit({
+    eventCategory: category,
+    eventLabel: label,
+    eventAction: action,
+    ...properties,
+  })
   // eslint-disable-next-line
   console.log(
     `category: ${category}\nlabel: ${label}\naction: ${action}\nproperties: ${JSON.stringify(
