b30a396f31c362b1af5d1e644e3a5bb947afd0aa
Alex Tewpin <alex.tewpin@gmail.com>
2019-09-27 18:48:23 +0300

    CallToAction

--- packages/york-web/src/components/complex/CallToAction/index.js
+++ packages/york-web/src/components/complex/CallToAction/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { GridContainer, GridColumn, Button } from 'york-web/components/simple'
+import { GridContainer, GridColumn } from 'york-web/components/simple'
 import { Text, Separator } from 'york-web/components/primitive'
 import { uiPoint, media } from 'york-web/utils'
 
-const StyledButtonContainer = styled.div`
+const StyledActionContainer = styled.div`
   width: ${uiPoint * 80}px;
 
   ${media.mobile(`
@@ -16,26 +16,14 @@ const StyledButtonContainer = styled.div`
 `
 
 /** Компонент призыва к действию */
-const CallToAction = ({
-  title,
-  description,
-  caption,
-  buttonProps,
-  rightNode,
-}) => (
-  <GridContainer
-    alignItems="center"
-    mobileProps={{ flexDirection: 'column-reverse' }}
-  >
+const CallToAction = ({ title, description, caption, action, rightNode }) => (
+  <GridContainer mobileProps={{ flexDirection: 'column-reverse' }}>
     <GridColumn columns={6} mobileProps={{ columns: 12 }}>
-      {title && (
-        <>
-          <Text preset="link" color="green">
-            {title}
-          </Text>
-          <Separator height={3} mobileProps={{ height: 1 }} />
-        </>
-      )}
+      <Separator height={3} mobileProps={{ height: 8 }} />
+      <Text preset="link" color="green">
+        {title}
+      </Text>
+      <Separator height={3} mobileProps={{ height: 1 }} />
       <Text
         preset="header3"
         wideProps={{ preset: 'header2' }}
@@ -44,15 +32,15 @@ const CallToAction = ({
         {description}
       </Text>
       <Separator height={8} mobileProps={{ height: 4 }} />
-      <StyledButtonContainer>
-        <Button {...buttonProps} />
+      <StyledActionContainer>
+        {action}
         {caption && (
           <>
             <Separator height={2} />
             <Text color="grey">{caption}</Text>
           </>
         )}
-      </StyledButtonContainer>
+      </StyledActionContainer>
     </GridColumn>
     <GridColumn
       columns={6}
@@ -61,26 +49,23 @@ const CallToAction = ({
       }}
     >
       {rightNode}
-      <Separator height={0} mobileProps={{ height: 8 }} />
     </GridColumn>
   </GridContainer>
 )
 
 CallToAction.defaultProps = {
-  title: null,
   caption: null,
 }
 
 CallToAction.propTypes = {
   /** Заголовок */
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
   /** Текст */
   description: PropTypes.string.isRequired,
+  /** Кнопка, ссылка или другой экшен */
+  action: PropTypes.node.isRequired,
   /** Подсказка под кнопкой */
   caption: PropTypes.string,
-  /** Пропы кнопки */
-  // eslint-disable-next-line react/forbid-prop-types
-  buttonProps: PropTypes.object.isRequired,
   /** Компонент, который рендерится справа */
   rightNode: PropTypes.node.isRequired,
 }
