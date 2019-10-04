f926ac1fcd7da94157d42186b4af97cb38ac79e5
Lev Ostanin <levkus@gmail.com>
2019-10-04 12:17:16 +0300

    remove utils

--- packages/york-analytics/src/utils/encodeAnalyticsEvent.js
+++
@@ -1,8 +0,0 @@
-export default function({ category, action, label, ...properties }) {
-  const params = `tstamp0=${Date.now()}&ec0=${encodeURI(
-    category,
-  )}&el0=${encodeURI(label)}&ea0=${encodeURI(action)}`
-  return Object.keys(properties).length > 0
-    ? `${params}&prp0=${encodeURI(JSON.stringify(properties))}`
-    : params
-}
