ef12e8708a3f7f74114c93d8852298aa3741e071
mlnchk <danil.mlnchk@gmail.com>
2019-12-10 13:05:34 +0300

    Fix href

--- packages/york-core/src/utils/formatters.js
+++ packages/york-core/src/utils/formatters.js
@@ -9,6 +9,7 @@ export const formatPhone = phone =>
       `${phone.startsWith('8') ? '' : '+'}$1\u00a0($2)\u00a0$3-$4-$5`,
     )
 
-export const formatPhoneHref = phone => `tel:+${phone.replace(/\D/g, '')}`
+export const formatPhoneHref = phone =>
+  `tel:${phone.startsWith('8') ? '' : '+'}${phone.replace(/\D/g, '')}`
 
 export const formatMoney = money => `${money}\u00a0р.`
