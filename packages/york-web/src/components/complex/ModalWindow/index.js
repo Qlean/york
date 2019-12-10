970b9a632714ee3b4bb08eca839da8599795ffd3
Alexander Tewpin <alex.tewpin@gmail.com>
2019-12-10 17:04:52 +0300

    off-2304-modal (#126)
    * Basic moda stuff
    
    * ModalWindow and more stuff
    
    * Body scroll lock
    
    * Modal docs
    
    * ModalWindow docs
    
    * Cleanup
    
    * Removed lots of styles from Modal
    
    * Docs
    
    * Minor styling
    
    * Sizes
    
    * Save body styles

---
+++ packages/york-web/src/components/complex/ModalWindow/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { Modal } from 'york-web/components/primitive'
+import { media, sizes } from 'york-web/utils'
+
+import Window from './Window'
+
+const StyledModal = styled(Modal)`
+  display: flex;
+  padding: ${sizes[20]}px 0;
+  box-sizing: border-box;
+  ${media.desktop(`
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+  `)}
+  ${media.mobile(`
+    padding: 0;
+  `)}
+`
+
+/**
+ * Расширенная версия компонента `Modal`, включающая в себя готовое окно с заголовком и кнопкой
+ * закрытия. Кроме указанных принимает все пропсы, которые принимает `Modal`.
+ */
+const ModalWindow = ({ title, size, children, ...rest }) => {
+  return (
+    <StyledModal {...rest}>
+      <Window title={title} size={size} onRequestClose={rest.onRequestClose}>
+        {children}
+      </Window>
+    </StyledModal>
+  )
+}
+
+ModalWindow.defaultProps = {
+  title: null,
+  size: 'm',
+}
+
+ModalWindow.propTypes = {
+  /** Заголовок окна. Если передан не элемент, то будет обернут в `<Text>` */
+  title: PropTypes.node,
+  /**
+   * Размер окна: `m` равен 6 колонкам, а `l` — 8 колонкам. Не влияет на размер в мобильных
+   * разрешениях.
+   */
+  size: PropTypes.oneOf(['m', 'l']),
+  /** Содержимое окна */
+  children: PropTypes.node.isRequired,
+}
+
+export default ModalWindow
