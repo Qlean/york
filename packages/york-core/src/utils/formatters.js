744860da76482dece362551cdf575b63f1b10551
mlnchk <danil.mlnchk@gmail.com>
2019-12-10 12:53:18 +0300

    analytics-95-global-phone

--- packages/york-core/src/utils/formatters.js
+++ packages/york-core/src/utils/formatters.js
@@ -6,7 +6,7 @@ export const formatPhone = phone =>
     .replace(/\D/g, '')
     .replace(
       /(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/,
-      '+$1\u00a0($2)\u00a0$3-$4-$5',
+      `${phone.startsWith('8') ? '' : '+'}$1\u00a0($2)\u00a0$3-$4-$5`,
     )
 
 export const formatPhoneHref = phone => `tel:+${phone.replace(/\D/g, '')}`
